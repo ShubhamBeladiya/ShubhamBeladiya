8c9e578dbe7c3d386201400100bee073e2e769a9
shubhambeladiya555-oss <beladiyashubham@gmail.com>
2025-10-29 20:23:47 +0530

    Update Profile.jsx

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -59,7 +59,7 @@ const Profile = () => {
             </a>
             <a
               className={`btn xxs:btn-lg px-6 max-xs:px-2 xxs:py-3 hover:border-picto-primary bg-white duration-300 transition-all hover:text-picto-primary  ms-4 text-xs xxs:text-[14px] sm:text-[16px]`}
-              href="/Shubham-Beladiya-Resume-2025.pdf"
+              href="./Shubham-Beladiya-Resume-2025.pdf"
               download
             >
               <FontAwesomeIcon icon={faDownload} /> Download CV
