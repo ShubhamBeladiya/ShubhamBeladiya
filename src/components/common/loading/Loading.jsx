2a49c2b00d2c8f551c383e40dbd588ae34cb55ba
shubhambeladiya555-oss <beladiyashubham@gmail.com>
2025-10-29 14:14:46 +0530

    Update Loading.jsx

--- src/components/common/loading/Loading.jsx
+++ src/components/common/loading/Loading.jsx
@@ -3,9 +3,10 @@ import { HashLoader } from "react-spinners";
 const Loading = () => {
   return (
     <div
-      className={`flex justify-center items-center fixed bottom-0 bg-white h-[100vh] w-[100vw] mx-auto z-50 `}
+      className={`flex justify-center items-center fixed bottom-0 h-[100vh] w-[100vw] mx-auto z-50 `}
+      style={{ backgroundColor: 'var(--color-soft-white)' }}
     >
-      <HashLoader color="#A53DFF" speedMultiplier={2} size={80} />
+      <HashLoader color="var(--color-picto-primary)" speedMultiplier={2} size={80} />
     </div>
   );
 };
